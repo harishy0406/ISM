7adf89b79ae89b338164f7e98deadbec8bdeae91
harishy0406 <harivelgm76@gmail.com>
2026-03-23 22:49:14 +0530

    AES-256-CBC + PKCS7 padding for content added

--- Digital_envolpe.py
+++ Digital_envolpe.py
@@ -4,8 +4,8 @@ Suitable for large messages / files
 
 Features:
 - RSA-OAEP for key encapsulation (2048-bit)
-- AES-256-CBC + PKCS7 padding for content
-- Base64 encoding for transmission 
+- AES-256-CBC + PKCS7 padding for content 
+- Base64 encoding for transmission  
 - Supports text input or file reading
 """
 
